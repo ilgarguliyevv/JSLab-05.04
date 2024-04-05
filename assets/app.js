90717a3337761420929d490b70b3fd1a62ea12b1
ilgarguliyevv <ilgarguliyev2004@gmail.com>
2024-04-05 17:01:20 +0400

    05.04 tasks

---
+++ assets/app.js
@@ -0,0 +1,118 @@
+
+
+// bosluqlari nezere almadan cumledeki soz saylari
+
+// let arr = prompt("Cumle daxil edin")
+
+// function Word(array) {
+//     let count = 0;
+//     let bosluq = arr.split(" ")
+//     for (let i = 0; i < bosluq.length; i++) {
+//         count++;
+
+//     }
+//     console.log(count);
+// }
+// Word(arr)
+
+// verilmis sozde sait herflerin tapilmasi
+
+// let soz = prompt("Sozu daxil edin")
+// let array = "aioue"
+// // let word = ["a", "i", "o", "u", "e"];
+// function Sait(soz) {
+//     let count = 0;
+//     for (let i = 0; i < soz.length; i++) {
+//         for (let j = 0; j < array.length; j++) {
+//             if (soz[i].toUpperCase() == array[j].toUpperCase()) {
+//                 count++;
+//             }
+
+//         }
+
+//     }
+//     console.log(count);
+// }
+
+// Sait(soz);  
+
+// // // // Product search
+
+
+// // // // let product1 = {
+// // // //     name: "Alma",
+// // // //     category: "Fruit",
+// // // //     price: "12.5",
+// // // //     weight: "50",
+// // // // };
+// // // // let product2 = {
+// // // //     name: "Armud",
+// // // //     category: "Fruit",
+// // // //     price: "15.0",
+// // // //     weight: "30",
+// // // // };
+// // // // let product3 = {
+// // // //     name: "Nar",
+// // // //     category: "Fruit",
+// // // //     price: "25.0",
+// // // //     weight: "18",
+// // // // };
+// // // // let product4 = {
+// // // //     name: "Sogan",
+// // // //     category: "Vegetable",
+// // // //     price: "1.5",
+// // // //     weight: "4",
+// // // // };
+// // // // let product5 = {
+// // // //     name: "Banan",
+// // // //     category: "Fruit",
+// // // //     price: "20.0",
+// // // //     weight: "10",
+// // // // };
+
+// // // // let array = [product1, product2, product3, product4, product5];
+
+// // // // let soz = prompt("Product daxil edin");
+
+// // // // function fruitsProduct(array) {
+// // // //     for (let i = 0; i < array.length; i++) {
+// // // //         if (array[i].name.toUpperCase().includes(soz.toUpperCase)) {
+// // // //             console.log(`Meyve ${array[i].name}
+
+// // // //             `);
+
+// // // //         }
+
+// // // //     }
+// // // // }
+
+// // // // fruitsProduct(soz, array)
+
+
+// Palindrom olub olmadligini yoxla
+
+// let array = prompt("Soz daxil edin:")
+
+// function numb(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if () {
+
+//         }
+
+//     }
+
+// }
+
+
+// function isPalindrome(word) {
+//     let reversedWord = word.split('').reverse().join('');
+//     return word === reversedWord;
+// }
+// let word = "radar";
+// console.log(isPalindrome(word));
+
+// word = "hello";
+// console.log(isPalindrome(word));
+
+
+
